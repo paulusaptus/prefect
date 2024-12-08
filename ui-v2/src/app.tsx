9485a1fc2b105196867cce0cfefb5ca67f1ccce3
Alex Streed <alex.s@prefect.io>
2024-12-07 21:37:24 -0600

    Add tests

--- ui-v2/src/app.tsx
+++ ui-v2/src/app.tsx
@@ -1,9 +1,10 @@
 import { RouterProvider } from "@tanstack/react-router";
-import { queryClient, router } from "./router";
-import { QueryClientProvider } from "@tanstack/react-query";
+import { router } from "./router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export const App = () => {
+	const queryClient = new QueryClient();
 	return (
 		<QueryClientProvider client={queryClient}>
 			<RouterProvider router={router} />
